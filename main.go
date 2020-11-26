fcf66e5d4b5e0725194e49bcd48b2d6b4ad92d32
kamesama503 <yujiayuthu@gmail.com>
2020-11-27 00:17:12 +0900

    step 00

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// initialize game
+
+	// loading resources
+
+	// game loop
+	for {
+		// update scteen
+
+		// process input
+
+		// process movement
+
+		// process collisions
+
+		// check game over
+		fmt.Println("Hello, world!")
+		break
+
+		// repeat
+	}
+}
